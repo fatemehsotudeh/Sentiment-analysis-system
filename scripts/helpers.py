d45534db3f0ef788acf15f1ba4d9459a617f519f
fatemehsotudeh <fatemehsotudeh20@gmail.com>
2023-12-14 00:11:31 +0330

    add exploratory analysis

---
+++ scripts/helpers.py
@@ -0,0 +1,8 @@
+import pandas as pd
+def read_file(file_path):
+    try:
+        df = pd.read_csv(file_path, encoding='ISO-8859-1')
+        return df
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return None
