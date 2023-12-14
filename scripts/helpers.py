a1fd1731a9d469f2731340cd2f5359732651bc6d
fatemehsotudeh <fatemehsotudeh20@gmail.com>
2023-12-14 18:24:01 +0330

    add preprocessing step

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -6,3 +6,6 @@ def read_file(file_path):
     except Exception as e:
         print(f"Error reading file: {e}")
         return None
+
+def save_to_csv(df, filename):
+    df.to_csv(filename, index=False)
